e04527fc833ebfc391961371f8d42cc3f66d6ec9
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-01-30 18:21:25 +0900

    ActivityとFragmentをレイアウトとバインディング

--- app/src/main/java/com/android/recyclerviewsample_java/MainActivity.java
+++ app/src/main/java/com/android/recyclerviewsample_java/MainActivity.java
@@ -1,14 +1,25 @@
 package com.android.recyclerviewsample_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.android.recyclerviewsample_java.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // レイアウト要素をバインディング
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        // Fragmentを表示
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(binding.layoutContainer.getId(), new RecyclerViewFragment())
+                .commit();
     }
 }
