2e6a5af6221bfdcca72ba3ba84883e4131410da6
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-02-01 09:11:43 +0900

    フォーム＋ボタンセルのViewDataを追加

---
+++ app/src/main/java/com/android/recyclerviewsample_java/list/FormButtonViewData.java
@@ -0,0 +1,32 @@
+package com.android.recyclerviewsample_java.list;
+
+import android.text.TextUtils;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.android.recyclerviewsample_java.BR;
+
+public class FormButtonViewData extends BaseObservable implements ViewData {
+
+    private String formText;
+
+    @Override
+    public int getType() {
+        return Type.FORM_BUTTON;
+    }
+
+    @Bindable public String getFormText() {
+        return formText;
+    }
+
+    public void setFormText(String formText) {
+        this.formText = formText;
+        notifyPropertyChanged(BR.buttonEnable);
+    }
+
+    // public 必須
+    @Bindable public boolean isButtonEnable() {
+        return !TextUtils.isEmpty(formText);
+    }
+}
--- app/src/main/java/com/android/recyclerviewsample_java/list/ViewData.java
+++ app/src/main/java/com/android/recyclerviewsample_java/list/ViewData.java
@@ -8,10 +8,11 @@ public interface ViewData {
 
     // 全体的にわからないことが多すぎる
     @Retention(RetentionPolicy.SOURCE)
-    @IntDef({ Type.HEADER, Type.TEXTS })
+    @IntDef({ Type.HEADER, Type.TEXTS, Type.FORM_BUTTON })
     @interface Type {
         int HEADER = 1;
         int TEXTS = 2;
+        int FORM_BUTTON = 3;
     }
 
     @Type int getType();
